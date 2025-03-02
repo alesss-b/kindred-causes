f9245632e0b3da8dde1ce2623817156a36539d4f
alesss-b <alexbradshaw21977@gmail.com>
2025-03-02 14:24:47 -0600

    Migrated database

--- kindred_causes/main/models.py
+++ kindred_causes/main/models.py
@@ -15,30 +15,25 @@ class Base(models.Model):
         abstract = True
 
 
-
-
 # Models:
 class Skill(Base):
     """ A skill that is required at an Event and that a Volunteer can have.
     """
     name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Name", db_comment="The name of the Skill.")
-    description = models.CharField(null=False, blank=False, verbose_name="Description", db_comment="A detailed description of the Skill.")
+    description = models.CharField(max_length=254, null=False, blank=False, verbose_name="Description", db_comment="A detailed description of the Skill.")
     def __str__(self):
         return self.name
-
 
 
 class Event(Base):
     """ An Event.
     """
     name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Name", db_comment="The name of the Event.")
-    description = models.CharField(null=False, blank=False, verbose_name="Description", db_comment="A detailed description of the Event.")
-    location = models.CharField(null=False, blank=False, verbose_name="Location", db_comment="The location of the event.")
+    description = models.CharField(max_length=254,null=False, blank=False, verbose_name="Description", db_comment="A detailed description of the Event.")
+    location = models.CharField(max_length=254,null=False, blank=False, verbose_name="Location", db_comment="The location of the event.")
     urgency = models.IntegerField(null=False, blank=False, default=EventUrgency.MEDIUM, choices=EventUrgency.choices, verbose_name="Urgency", db_comment="The urgency of the event.")
     date = models.DateTimeField(null=True, blank=True, verbose_name="Date", db_comment="The date and time of the Event.")
     required_skills = models.ManyToManyField(Skill)
 
     def __str__(self):
         return self.name
-    
-
