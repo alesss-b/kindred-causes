d78da0906787519fe303e2bcdb0c851423d429c7
Chongyi Wang <mordredwang@gmail.com>
2025-03-02 13:30:48 -0600

    Change the views functions on account and account management

--- kindred_causes/main/views.py
+++ kindred_causes/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 
 
@@ -106,8 +106,23 @@ def account(request: HttpRequest) -> HttpResponse:
     :param HttpRequest reqest: The request from the client's browser.
     :return HttpReponse: The response to the client.
     """
-    context: dict = {'test_key': 'test_value'}
-    return render(request, 'account.html', context)
+    context = {
+        # Public
+        "name": request.session.get("name", "John Doe"),
+        "city": request.session.get("city", "New York"),
+        "state": request.session.get("state", "NY"),
+        "start_availability": request.session.get("availability_start", "January 1, 1989"),
+        "end_availability": request.session.get("availability_end", "January 1, 1999"),
+        "skills": ", ".join(request.session.get("skills", ["Eating", "Sleeping", "Programming"])),
+
+        # Private
+        "email": request.session.get("email", "john.doe@example.com"),
+        "address": request.session.get("address1", "123 Main St"),
+        "address2": request.session.get("address2", "None"),
+        "zip_code": request.session.get("zipcode", "11223"),
+        "phone": request.session.get("phone", "(123) 456-7890"),
+    }
+    return render(request, "account.html", context)
 
 def account_management(request: HttpRequest) -> HttpResponse:
     """ Account page.
@@ -115,8 +130,27 @@ def account_management(request: HttpRequest) -> HttpResponse:
     :param HttpRequest reqest: The request from the client's browser.
     :return HttpReponse: The response to the client.
     """
-    context: dict = {'test_key': 'test_value'}
-    return render(request, 'profile_management.html', context)
+    if request.method == "POST":
+        # Public
+        request.session["name"] = request.POST.get("name", "John Doe")
+        request.session["city"] = request.POST.get("city", "New York")
+        request.session["state"] = request.POST.get("state", "NY")
+        request.session["start_availability"] = request.POST.get("availability_start", "January 1, 1989")
+        request.session["end_availability"] = request.POST.get("availability_end", "January 1, 1999")
+        request.session["skills"] = request.POST.getlist("skills")
+
+        # Private
+        request.session["email"] = request.POST.get("email", "john.doe@example.com")
+        request.session["address"] = request.POST.get("address1", "123 Main St")
+        request.session["address2"] = request.POST.get("address2", "None")
+        request.session["zip_code"] = request.POST.get("zipcode", "11223")
+        request.session["phone"] = request.POST.get("phone", "(123) 456-7890")
+
+        request.session.modified = True
+
+        return redirect("account")
+    
+    return render(request, "profile_management.html")
 
 def index(request: HttpRequest) -> HttpResponse:
     """ Default page.
