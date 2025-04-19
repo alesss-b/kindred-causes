e2d4d9a257e2825952f010dc30608b5fc2b4ccf6
alesss-b <alexbradshaw21977@gmail.com>
2025-04-19 14:23:06 -0500

    Changed redirect after registration

--- kindred_causes/kindred_causes/views.py
+++ kindred_causes/kindred_causes/views.py
@@ -8,7 +8,7 @@ from .forms import UserRegistrationForm
 class RegisterView(FormView):
     template_name = 'registration/register.html'
     form_class = UserRegistrationForm
-    success_url = reverse_lazy('home')  # or whatever your landing page is
+    success_url = reverse_lazy('account_management')
 
     def form_valid(self, form):
         user = form.save()
